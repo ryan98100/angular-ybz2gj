0c0ed4da8c7e03d32e47af330f7676c95cdf1e31
ryan98100 <c14180115@john.petra.ac.id>
2020-04-29 08:29:32 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,54 +8,68 @@ import { Router } from '@angular/router';
 export class AppComponent  {
   data = "";
   temp="";
+  tampil="";
   hasil=0;
   lambang = "";
   name = 'Angular';
   constructor(private router : Router){}
   enol(){
     this.data += 0;
+    this.tampil += 0;
   }
   satu(){
     this.data += 1;
+     this.tampil += 1;
   }
   dua(){
     this.data += 2;
+     this.tampil += 2;
   }
   tiga(){
     this.data += 3;
+     this.tampil += 3;
   }
   empat(){
     this.data += 4;
+     this.tampil += 4;
   }
   lima(){
     this.data += 5;
+     this.tampil += 5;
   }
   enam(){
     this.data += 6;
+     this.tampil += 6;
   }
   tujuh(){
     this.data += 7;
+     this.tampil += 7;
   }
   delapan(){
     this.data += 8;
+     this.tampil += 8;
   }
   sembilan(){
     this.data += 9;
+     this.tampil += 9;
   }
   tambah(){
     this.temp = this.data;
     this.data = "";
     this.lambang = "+";
+     this.tampil += "+";
   }
   kurang(){
     this.temp = this.data;
     this.data = "";
     this.lambang = "-";
+     this.tampil += "-";
   }
   kali(){
     this.temp = this.data;
     this.data = "";
     this.lambang = "*";
+     this.tampil += "*";
   }
   hasile(){
     if (this.lambang == "+"){
@@ -68,7 +82,7 @@ export class AppComponent  {
        this.hasil = parseInt(this.temp) - parseInt(this.data);
     }
     alert(this.hasil);
-   
+    this.tampil = String(this.hasil);
     this.router.navigate(['/page1',this.hasil]);
   }
 }
